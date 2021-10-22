cc70064b7f9bee12de83f2ff4dd55fe1bc7bba87
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2021-10-22 18:05:37 +0530

    Applied react lazy to load components when needed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import React, { Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
 
-import AllQuotes from './pages/AllQuotes';
-import NewQuotes from './pages/NewQuote';
-import NotFound from './pages/NotFound';
-import QuoteDetails from './pages/QuoteDetails';
+const NewQuotes = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetails = React.lazy(() => import('./pages/QuoteDetails'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
 
 const App = () => {
     return (
         <Layout>
-            <Switch>
-                <Route exact path='/'>
-                    <Redirect to='/quotes' />
-                </Route>
-                <Route exact path='/quotes' component={AllQuotes} />
-                <Route path='/quotes/:quoteId' component={QuoteDetails} />
-                <Route path='/new-quote' component={NewQuotes} />
-                <Route path='*' component={NotFound} />
-            </Switch>
+            <Suspense
+                fallback={
+                    <div className='centered'>
+                        <LoadingSpinner />
+                    </div>
+                }
+            >
+                <Switch>
+                    <Route exact path='/'>
+                        <Redirect to='/quotes' />
+                    </Route>
+                    <Route exact path='/quotes' component={AllQuotes} />
+                    <Route path='/quotes/:quoteId' component={QuoteDetails} />
+                    <Route path='/new-quote' component={NewQuotes} />
+                    <Route path='*' component={NotFound} />
+                </Switch>
+            </Suspense>
         </Layout>
     );
 };
